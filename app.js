9403dae0fd9a6e20678cc8c04dccdf27e36b3a83
Patricia Mieres Díaz <patriciamieres3003@gmail.com>
2025-04-14 20:54:44 -0400

    Redesign

--- app.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('pdf-download').addEventListener('click', function() {
-    const link = document.createElement('a');
-    link.href = './media/Hola.pdf'; // Reemplaza con la ruta de tu archivo PDF
-    link.download = 'Hola.pdf'; // Nombre del archivo que se descargará
-    link.click();
-});
